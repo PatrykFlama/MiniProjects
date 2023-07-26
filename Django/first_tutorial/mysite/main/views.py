23c13965f3e6b99837bf9af186a0def2085ce5a1
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-07-26 22:52:03 +0200

    tutorial finished

--- Django/first_tutorial/mysite/main/views.py
+++ Django/first_tutorial/mysite/main/views.py
@@ -10,26 +10,29 @@ def home(response):
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)        # get the object with given id
-    if response.method == "POST":
-        print(response.POST)                # debug
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+
+    if ls in response.user.todolist.all():  # check if this todolist belongs to active user
+        if response.method == "POST":
+            print(response.POST)                # debug
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            
+            elif response.POST.get("newItem"):
+                text = response.POST.get("newText")
+
+                if len(text) > 2:
+                    ls.item_set.create(text=text, complete=False)
                 else:
-                    item.complete = False
-                item.save()
+                    print("Invalid")
         
-        elif response.POST.get("newItem"):
-            text = response.POST.get("newText")
+        return render(response, "main/list.html", {"ls":ls})
 
-            if len(text) > 2:
-                ls.item_set.create(text=text, complete=False)
-            else:
-                print("Invalid")
-        
-
-    return render(response, "main/list.html", {"ls":ls})
+    return render(response, "main/view.html", {})
 
 
 def create(response):
@@ -40,9 +43,14 @@ def create(response):
             name = form.cleaned_data["name"]
             todo = ToDoList(name=name)
             todo.save()
+            response.user.todolist.add(todo)
 
         return HttpResponseRedirect("/%i" %todo.id)
     else:
         form = CreateNewList()
 
     return render(response, "main/create.html", {"form":form})
+
+
+def view(response):
+    return render(response, "main/view.html", {})
