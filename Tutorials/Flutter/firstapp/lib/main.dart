929f876362de05d603abb089d53e52980f0e5319
PatrykFlama <patrykflama@interia.pl>
2023-09-09 21:52:45 +0200

    containers

--- Tutorials/Flutter/firstapp/lib/main.dart
+++ Tutorials/Flutter/firstapp/lib/main.dart
@@ -28,45 +28,64 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 82, 169, 0),
       ),
-      body: Center(
-        // ---- BUTTONS ----
-        child: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.alternate_email),
-        ),
 
-        // child: ElevatedButton.icon(
-        //   onPressed: () {},
-        //   icon: Icon(Icons.mail),
-        //   label: Text('send mail'),
-        // ),
-
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     print('WOW');
-        //   },
-        //   child: Icon(Icons.ads_click),
-        //   style: ButtonStyle(
-        //     backgroundColor: MaterialStatePropertyAll(Colors.purple),
-        //   ),
-        // ),
-
-        // ----- ICONS -----
-        // child: Icon(
-        //   Icons.macro_off,
-        //   color: Colors.purple,
-        //   size: 50.0,
-        // ),
-
-        // ----- IMAGES -----
-        // child: Image.asset('assets/image1.jpg'),
-
-        // child: Image.network(
-        //     'https://images.unsplash.com/photo-1564754943164-e83c08469116?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVydGljYWx8ZW58MHx8MHx8fDA%3D&w=1000&q=80'),
-
-        // ----- TEXT -----
-        // child: Text("Hello World!"),
+      body: Container(
+        // padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+        // padding: EdgeInsets.fromLTRB(5.0, 15.0, 30.0, 50.0),
+        margin: EdgeInsets.all(20.0),
+        color: Color.fromARGB(255, 138, 230, 255),
+        child: Text('hello'),
       ),
+
+      // body: Padding(     // only padding can be applied
+      //   padding: EdgeInsets.all(100.0),
+      //   child: Text("HI!"),
+      // ),
+
+      // body: Center(
+      //   // ---- BUTTONS ----
+      //   child: IconButton(
+      //     onPressed: () {},
+      //     icon: Icon(Icons.alternate_email),
+      //     style: ButtonStyle(
+      //       shadowColor: MaterialStatePropertyAll(Colors.red),
+      //       iconColor: MaterialStatePropertyAll(Colors.blue),
+      //     ),
+      //   ),
+
+      // child: ElevatedButton.icon(
+      //   onPressed: () {},
+      //   icon: Icon(Icons.mail),
+      //   label: Text('send mail'),
+      // ),
+
+      // child: ElevatedButton(
+      //   onPressed: () {
+      //     print('WOW');
+      //   },
+      //   child: Icon(Icons.ads_click),
+      //   style: ButtonStyle(
+      //     backgroundColor: MaterialStatePropertyAll(Colors.purple),
+      //   ),
+      // ),
+
+      // ----- ICONS -----
+      // child: Icon(
+      //   Icons.macro_off,
+      //   color: Colors.purple,
+      //   size: 50.0,
+      // ),
+
+      // ----- IMAGES -----
+      // child: Image.asset('assets/image1.jpg'),
+
+      // child: Image.network(
+      //     'https://images.unsplash.com/photo-1564754943164-e83c08469116?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dmVydGljYWx8ZW58MHx8MHx8fDA%3D&w=1000&q=80'),
+
+      // ----- TEXT -----
+      // child: Text("Hello World!"),
+      //   ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: const Color.fromARGB(255, 82, 169, 0),
