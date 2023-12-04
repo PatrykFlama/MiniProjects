80006ede2325f6f036bd0ebcc03d5fa0988202eb
PatrykFlama <51850536+PatrykFlama@users.noreply.github.com>
2023-12-04 12:39:17 +0100

    warning! everything in python

--- NIFS3/sandbox.py
+++ NIFS3/sandbox.py
@@ -5,12 +5,32 @@ from matplotlib.backend_bases import MouseButton
 import os
 import subprocess
 
+"""
+cpp script is faster (and better tested), but requires calc_nifs3.cpp to be compiled to calc_nifs3.exe
+python is python
+"""
+USE_CPP = False
+
 fig = plt.figure()
 ax = plt.gca()
 ax.set_aspect('equal', adjustable='box')
 
 class NIFS3:
-    def get_nifs3(points):
+    def __init__(self):
+        self.x0 = []
+        self.y0 = []
+        self.n = 0
+        self.t = []
+        self.Mx = []
+        self.My = []
+
+    def get_nifs3(self, points):
+        if USE_CPP:
+            return self.calc_nifs3_cpp(points)
+        else:
+            return self.calc_nifs3_py(points)
+    
+    def calc_nifs3_cpp(self, points):
         x = []
         y = []
 
@@ -31,6 +51,76 @@ class NIFS3:
             y.append(float(b))
 
         return x, y
+    
+    def calc_nifs3_py(self, points):
+        self.x0 = []
+        self.y0 = []
+        for x, y in points:
+            self.x0.append(x)
+            self.y0.append(y)
+        
+        self.nifs3_update()
+
+        res_x = []
+        res_y = []
+        T = self.t[0]
+        while T <= self.t[len(self.t)-1]:
+            res_x.append(self.calc_nifs3(T, self.x0, self.Mx))
+            res_y.append(self.calc_nifs3(T, self.y0, self.My))
+            T += 0.001
+
+        return res_x, res_y
+
+    def nifs3_update(self):
+        self.n = len(self.x0)-1
+        
+        self.t = []
+        for i in range(len(self.x0)):
+            self.t.append(i/self.n)
+
+        self.Mx = self.calc_M(self.x0)
+        self.My = self.calc_M(self.y0)
+    
+    def calc_M(self, x):
+        q = [0]
+        p = [0]
+        u = [0]
+
+        k = 1
+        while k <= self.n-1:
+            lk = self.calc_lam(k)
+            p.append(lk * q[k-1] + 2)
+            q.append((lk-1)/p[k])
+            u.append((self.dk(k, x) - lk * u[k-1])/p[k])
+            k += 1
+        
+        M = [0] * (self.n+1)
+        M[self.n] = 0
+        M[self.n-1] = u[self.n-1]
+        k = self.n-2
+        while k >= 0:
+            M[k] = u[k] - q[k] * M[k+1]
+            k -= 1
+        
+        return M
+    
+    def calc_lam(self, k):
+        return (self.t[k] - self.t[k-1])/(self.t[k+1] - self.t[k-1])
+    
+    def dk(self, k, x):
+        t1 = (x[k+1] - x[k])/(self.t[k+1] - self.t[k])
+        t2 = (x[k] - x[k-1])/(self.t[k] - self.t[k-1])
+        return 6 * (t1 - t2)/(self.t[k+1] - self.t[k-1])
+    
+    def calc_nifs3(self, X, x0, M):
+        k = 1
+        while self.t[k] < X: k += 1
+
+        return (1/(self.t[k]-self.t[k-1])) \
+            * (M[k-1]*pow(self.t[k] - X, 3)/6.
+            +  M[k] * pow(X - self.t[k-1], 3)/6. 
+            + (x0[k-1] - M[k-1]*pow(self.t[k]-self.t[k-1], 2)/6.)*(self.t[k]-X)
+            + (x0[k] - M[k]*pow(self.t[k]-self.t[k-1], 2)/6.)*(X-self.t[k-1]));
 
 
 class GUI:
@@ -39,6 +129,7 @@ class GUI:
         self.done = False
         self.mark_dots = True
         self.draw = True
+        self.nifs3 = NIFS3()
 
         plt.cla()
         self.image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -49,7 +140,7 @@ class GUI:
 
         print("Left-click to add point, right-click to remove point\n"+
               "Enter to separate point groups, backspace to remove last group\n"+
-              "m to enable/disable marking dots\n"+
+              "m to enable/disable marking points\n"+
               "d to enable/disable drawing\n"+
               "Escape to end")
     
@@ -76,10 +167,10 @@ class GUI:
     def update_view(self):
         plt.cla()
         plt.imshow(self.image, cmap='gray', vmin=0, vmax=255)
-        
+
         for c in self.coordinates:
             if(len(c) > 2):
-                x, y = NIFS3.get_nifs3(c)
+                x, y = self.nifs3.get_nifs3(c)
                 if self.mark_dots: plt.scatter(*zip(*c), color = "red")
                 ax.plot(x, y, '-')
             else:
@@ -91,11 +182,15 @@ class GUI:
     def start(self):
         while not self.done:
             plt.pause(0.001)
+            if not plt.fignum_exists(1):
+                self.done = True
         return self.coordinates
 
 gui = GUI("text.png")
 res = gui.start()
 
+if not os.path.exists("./points"):
+    os.makedirs("./points")
 for file in os.listdir("./points"):
     if file.endswith(".in"):
         os.remove(f"points/{file}")
